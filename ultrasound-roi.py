7397b84937bea80bf16970daa09a68b09623f271
Danny Jeng <jeng.danny@gmail.com>
2019-05-20 20:29:36 -0400

    Added new method get_mask to class ROIPolygon

--- ultrasound-roi.py
+++ ultrasound-roi.py
@@ -10,18 +10,29 @@ import matplotlib.patches as patches
 
 class ROIPolygon(object):
 
-	def __init__(self, ax):
+	def __init__(self, ax, rows, col):
 		self.canvas = ax.figure.canvas
 		self.poly = PolygonSelector(ax,
 									self.onselect,
 									lineprops = dict(color = 'g', alpha = 1),
 									markerprops = dict(mec = 'g', mfc = 'g', alpha = 1))
 		self.path = None
+		self.mask = np.zeros([rows, col], dtype = int)
 
 	def onselect(self, verts):
 		path = Path(verts)
 		self.canvas.draw_idle()
 		self.path = path
+
+	def get_mask(self, rows, col):
+		for i in range(rows):
+			for j in range(col):
+				# matplotlib.Path.contains_points returns True if the point is inside the ROI
+				# Path vertices are considered in different order than numpy arrays
+				 if self.path.contains_points([(j,i)]) == [True]:
+					 self.mask[i][j] = 1
+				 else:
+					 self.mask[i][j] = 0
 
 def rgb2gray(rgb):
 	# from stackoverflow
@@ -35,6 +46,10 @@ ds = pydicom.dcmread(sys.argv[1])
 frame_n = int(input("Enter desired frame to view: "))
 img = ds.pixel_array[frame_n]
 
+rows = ds.pixel_array.shape[1] # number of rows of pixels in image
+col = ds.pixel_array.shape[2] # number of columns of pixels in image
+
+
 # Drawing ROI on selected frame
 q = 'n'
 while q == 'n' or q == 'N':
@@ -43,7 +58,7 @@ while q == 'n' or q == 'N':
 	plt.show(block = False)
 
 	print('Draw desired ROI')
-	roi1 = ROIPolygon(ax)
+	roi1 = ROIPolygon(ax, rows, col)
 
 	plt.imshow(img)
 	plt.show()
@@ -66,25 +81,12 @@ while q == 'n' or q == 'N':
 			print('Please enter y or n')
 
 # Drawing mask
-rows = ds.pixel_array.shape[1] # number of rows of pixels in image
-col = ds.pixel_array.shape[2] # number of columns of pixels in image
-
-mask = np.zeros([rows, col], dtype = int)
-
-for i in range(rows):
-	for j in range(col):
-		# matplotlib.Path.contains_points returns True if the point is inside the ROI
-		# Path vertices are considered in different order than numpy arrays
-		 if roi1.path.contains_points([(j,i)]) == [True]:
-			 mask[i][j] = 1
-		 else:
-			 mask[i][j] = 0
-
+mask_test = roi1.get_mask(rows, col)
 masked_ds = np.zeros([rows, col, 3]) # empty matrix to fill in
 
 for i in range(rows):
 	for j in range(col):
-		if mask[i][j] == 1:
+		if roi1.mask[i][j] == 1:
 			masked_ds[i][j] = ds.pixel_array[frame_n][i][j]
 		else:
 			masked_ds[i][j] = 0
@@ -100,7 +102,7 @@ plt.subplot(132)
 plt.imshow(gray_masked_ds, cmap = 'gray')
 
 plt.subplot(133)
-plt.imshow(mask)
+plt.imshow(roi1.mask)
 plt.show()
 
 print(masked_ds.shape)
