c4144c5b9d2adf9377e48ffbe1db69fd8f6ad9c8
Danny Jeng <jeng.danny@gmail.com>
2019-05-21 20:12:59 -0400

    Changed get_mask from method to function and further condensed it.

--- ultrasound-roi.py
+++ ultrasound-roi.py
@@ -1,5 +1,6 @@
 # for reading DICOM files obtained from ultrasound machines,
 # letting user draw ROI, and outputting intensity-time graph
+import time
 
 import sys
 import matplotlib.pyplot as plt
@@ -18,26 +19,27 @@ class ROIPolygon(object):
 									lineprops = dict(color = 'g', alpha = 1),
 									markerprops = dict(mec = 'g', mfc = 'g', alpha = 1))
 		self.path = None
-		# self.masked_dcm = np.zeros([row, col, 3])
 
 	def onselect(self, verts):
 		path = Path(verts)
 		self.canvas.draw_idle()
 		self.path = path
 
-	def get_mask(self, row, col, dcm_vid, frame_n):
-		masked_dcm = np.zeros([row, col, 3])
-		for i in np.arange(row):
-			for j in np.arange(col):
-				# matplotlib.Path.contains_points returns True if the point is inside the ROI
-				# Path vertices are considered in different order than numpy arrays
-				 if self.path.contains_points([(j,i)]) == [True]:
-					 # self.masked_dcm[i][j] = dcm_vid.pixel_array[frame_n][i][j]
-					 masked_dcm[i][j] = dcm_vid.pixel_array[frame_n][i][j]
-				 else:
-					 # self.masked_dcm[i][j] = 0
-					 masked_dcm[i][j] = 0
-		return masked_dcm
+# To create mask on dicom file using drawn ROI
+def get_mask(drawn_roi, row, col, dcm_vid, frame_n):
+	# matplotlib.Path.contains_points returns True if the point is inside the ROI
+	# Path vertices are considered in different order than numpy arrays
+	#
+	# Variables :
+	# drawn_roi : ROI created using ROIPolygon
+	masked_dcm = np.zeros([row, col, 3])
+	drawn_roi_path = drawn_roi.path
+	dcm_vid_framed = dcm_vid[frame_n]
+	for i in np.arange(row):
+		for j in np.arange(col):
+			if drawn_roi_path.contains_points([(j,i)]) == True:
+				 masked_dcm[i][j] = dcm_vid_framed[i][j]
+	return masked_dcm
 
 # Converting rgb image to grayscale.
 def rgb2gray(rgb):
@@ -45,14 +47,16 @@ def rgb2gray(rgb):
 
 # Reading the .dcm file. Pixel data accessed via pixel_array and is stored as a
 # 4D numpy array - (frame number, rows, columns, rgb channel)
-dcm = pydicom.dcmread(sys.argv[1])
+d_pix_arr = pydicom.dcmread(sys.argv[1]).pixel_array
+
 frame = int(input("Enter desired frame to view: "))
-r = dcm.pixel_array.shape[1] # number of rows of pixels in image
-c = dcm.pixel_array.shape[2] # number of columns of pixels in image
+
+r = d_pix_arr.shape[1] # number of rows of pixels in image
+c = d_pix_arr.shape[2] # number of columns of pixels in image
 
 # Drawing ROI on selected frame. If ROI is decided by user to be incorrect,
 # they have the option to redraw.
-img = dcm.pixel_array[frame]
+img = d_pix_arr[frame]
 q = 'n'
 while q == 'n' or q == 'N':
 	fig, ax = plt.subplots()
@@ -84,9 +88,9 @@ while q == 'n' or q == 'N':
 
 # The previously drawn ROI is used to create a mask to extract the ROI from the
 # original .dcm frame
-# roi.get_mask(r, c, dcm, frame)
-# gray_masked_dcm = rgb2gray(roi.masked_dcm)
-gray_masked_dcm = rgb2gray(roi.get_mask(r, c, dcm, frame))
+start_time = time.time()
+gray_masked_dcm = rgb2gray(get_mask(roi, r, c, d_pix_arr, frame))
+print("--- %s seconds to run get_mask---" % (time.time() - start_time))
 
 # Test plots and info
 plt.figure(1)
@@ -95,25 +99,22 @@ plt.imshow(img)
 
 plt.subplot(122)
 plt.imshow(gray_masked_dcm, cmap = 'gray')
-
-# plt.subplot(133)
-# plt.imshow(roi.masked_dcm)
 plt.show()
 
 mean_masked_dcm = np.mean(gray_masked_dcm)
 print(mean_masked_dcm)
 
 # Loop through all frames in video
-gray_dcm_mean = []
-number_frames = dcm.pixel_array.shape[0]
-index = 0
-for i in np.arange(number_frames):
-	# roi.get_mask(r, c, dcm, i)
-	g = rgb2gray(roi.get_mask(r, c, dcm, i))
-	avg = np.mean(g)
-	gray_dcm_mean.append(avg)
-	index += 1
-	print(index)
+# gray_dcm_mean = []
+# number_frames = dcm.pixel_array.shape[0]
+# index = 0
+# for i in np.arange(number_frames):
+# 	# roi.get_mask(r, c, dcm, i)
+# 	g = rgb2gray(get_mask(roi_path, r, c, dcm, i))
+# 	avg = np.mean(g)
+# 	gray_dcm_mean.append(avg)
+# 	index += 1
+# 	print(index)
 
 # Plot the mean vs frames
 # plt.plot(np.arange(number_frames), gray_dcm_mean)
