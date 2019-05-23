7cdc8d25425a8841a2b68ef1ce0f4304c35bf940
Danny Jeng <jeng.danny@gmail.com>
2019-05-22 22:22:21 -0400

    Created draw_roi function and mask initialization

--- ultrasound-roi.py
+++ ultrasound-roi.py
@@ -25,62 +25,16 @@ class ROIPolygon(object):
 		self.canvas.draw_idle()
 		self.path = path
 
-# To create mask on dicom file using drawn ROI
-def get_mask(drawn_roi, row, col, dcm_vid, frame_n):
-	# matplotlib.Path.contains_points returns True if the point is inside the ROI
-	# Path vertices are considered in different order than numpy arrays
-	#
-	# Variables :
-	# drawn_roi : ROI created using ROIPolygon
-
-	masked_dcm = np.zeros([row, col, 3])
-	drawn_roi_path = drawn_roi.path
-	dcm_vid_framed = dcm_vid[frame_n]
-	for i in np.arange(row):
-		for j in np.arange(col):
-			if drawn_roi_path.contains_points([(j,i)]) == True:
-				 masked_dcm[i][j] = dcm_vid_framed[i][j]
-	# for i,j in np.ndindex(row, col):
-	# 	if drawn_roi_path.contains_points([(j,i)]) == True:
-	# 		masked_dcm[i][j] = dcm_vid_framed[i][j]
-	# mask = np.zeros([row, col], dtype = int)
-	# for i in np.arange(row):
-	# 	for j in np.arange(col):
-	# 		 if drawn_roi_path.contains_points([(j,i)]) == [True]:
-	# 			 mask[i][j] = 1
-	# mask_swap_axes = np.swapaxes(mask,0,1)
-	# masked_img = dcm_vid_framed[np.where(mask_swap_axes == 1)]
-	# return masked_img
-	return masked_dcm
-
-# Converting rgb image to grayscale.
-def rgb2gray(rgb):
-	return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
-
-# Reading the .dcm file. Pixel data accessed via pixel_array and is stored as a
-# 4D numpy array - (frame number, rows, columns, rgb channel)
-d_pix_arr = pydicom.dcmread(sys.argv[1]).pixel_array
-
-frame = int(input("Enter desired frame to view: "))
-
-r = d_pix_arr.shape[1] # number of rows of pixels in image
-c = d_pix_arr.shape[2] # number of columns of pixels in image
-
-# Drawing ROI on selected frame. If ROI is decided by user to be incorrect,
-# they have the option to redraw.
-img = d_pix_arr[frame]
-q = 'n'
-while q == 'n' or q == 'N':
+def draw_roi(img, row, col):
 	fig, ax = plt.subplots()
 	plt.imshow(img)
 	plt.show(block = False)
-
 	print('Draw desired ROI')
-	roi = ROIPolygon(ax, r, c)
+
+	roi = ROIPolygon(ax, row, col)
 
 	plt.imshow(img)
 	plt.show()
-
 	# Overlaying ROI onto image
 	patch = patches.PathPatch(roi.path,
 							facecolor = 'green',
@@ -89,35 +43,78 @@ while q == 'n' or q == 'N':
 	plt.imshow(img)
 	ax.add_patch(patch)
 	plt.show(block = False)
+	return roi
 
+# To initialize mask
+def get_mask(drawn_roi, row, col):
+	# matplotlib.Path.contains_points returns True if the point is inside the ROI
+	# Path vertices are considered in different order than numpy arrays
+	#
+	# Variables :
+	# drawn_roi : ROI created using function draw_roi
+	#
+	mask = np.zeros([row, col], dtype = int)
+	for i in np.arange(row):
+		for j in np.arange(col):
+			 if drawn_roi.path.contains_points([(j,i)]) == [True]:
+				 mask[i][j] = 1
+	return mask
+
+	# masked_dcm = np.zeros([row, col, 3])
+	# drawn_roi_path = drawn_roi.path
+	# dcm_vid_framed = dcm_vid[frame_n]
+	# for i in np.arange(row):
+	# 	for j in np.arange(col):
+	# 		if drawn_roi_path.contains_points([(j,i)]) == True:
+	# 			 masked_dcm[i][j] = dcm_vid_framed[i][j]
+	# return masked_dcm
+
+# Converting rgb image to grayscale.
+def rgb2gray(rgb):
+	return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+# Reading the .dcm file. Pixel data accessed via pixel_array and is stored as a
+# 4D numpy array - (frame number, rows, columns, rgb channel)
+d_pix_arr = pydicom.dcmread(sys.argv[1]).pixel_array
+frame = int(input("Enter desired frame to view: "))
+r = d_pix_arr.shape[1] # number of rows of pixels in image
+c = d_pix_arr.shape[2] # number of columns of pixels in image
+img = d_pix_arr[frame]
+q = 'n'
+# Draw correct ROI
+while q == 'n' or q == 'N':
+	roi = draw_roi(img, r, c)
 	q = input("Is this ROI correct? y/n: ")
-
 	while True:
 		if q == 'y' or q == 'Y' or q == 'n' or q == 'N':
 			break
 		else:
 			print('Please enter y or n')
-
-# The previously drawn ROI is used to create a mask to extract the ROI from the
-# original .dcm frame
+# Initialize mask
 start_time = time.time()
-# mask = np.zeros([r,c])
-# for i in np.arange(r):
-# 	for j in np.arange(c):
-# 		mask[i][j] = roi.path.contains_points([(j,i)]) #writes True as 1, False as 0
-# print(roi.path.contains_points([(200,300),[100,300]]))
-gray_masked_dcm = rgb2gray(get_mask(roi, r, c, d_pix_arr, frame))
-
-# Loop through all frames in video
-# gray_dcm_mean = []
-# number_frames = d_pix_arr.shape[0]
-# index = 0
-# for i in np.arange(number_frames):
-# 	g = rgb2gray(get_mask(roi, r, c, d_pix_arr, i))
-# 	avg = np.mean(g)
-# 	gray_dcm_mean.append(avg)
-# 	index += 1
-# 	print(index)
+mask = get_mask(roi, r, c)
+# Use mask to extract correct info
+masked_dcm = np.zeros([r, c, 3])
+dcm_vid_framed = d_pix_arr[frame]
+frame_all = d_pix_arr.shape[0]
+grayed = np.zeros([frame_all, r, c])
+grayed_avg = np.zeros([frame_all, 1])
+for n in np.arange(frame_all):
+	for i in np.arange(r):
+		for j in np.arange(c):
+			if mask[i][j] == 1:
+			 	# masked_dcm[i][j] = dcm_vid_framed[i][j]
+				masked_dcm[i][j] = d_pix_arr[n][i][j]
+	g_frame = rgb2gray(masked_dcm)
+	g_frame_avg = np.mean(g_frame)
+	grayed[n] = g_frame
+	grayed_avg[n] = g_frame_avg
+# gray_masked_dcm = rgb2gray(masked_dcm)
+# print(gray_masked_dcm.shape)
+print(grayed.shape)
+print(grayed_avg.shape)
+plt.plot(np.arange(frame_all),grayed_avg)
+plt.show()
 
 print("--- %s seconds to run get_mask in a loop---" % (time.time() - start_time))
 # gray_masked_dcm = rgb2gray(masked_dcm)
@@ -128,32 +125,12 @@ plt.subplot(121)
 plt.imshow(img)
 
 plt.subplot(122)
-plt.imshow(gray_masked_dcm, cmap = 'gray')
+plt.imshow(grayed[1], cmap = 'gray')
 plt.show()
-
-mean_masked_dcm = np.mean(gray_masked_dcm)
-print(mean_masked_dcm)
+#
+# mean_masked_dcm = np.mean(grayed[1])
+# print(mean_masked_dcm)
 
 # Plot the mean vs frames
 # plt.plot(np.arange(number_frames), gray_dcm_mean)
 # plt.show()
-
-## get_mask function taken out into own loop
-# start_time = time.time()
-# masked_dcm = np.zeros([r, c, 3])
-# drawn_roi_path = roi.path
-# gray_loop = []
-# index = 0
-# all_frame = d_pix_arr.shape[0]
-# for k in np.arange(all_frame):
-# 	dcm_vid_framed = d_pix_arr[k]
-# 	for i in np.arange(r):
-# 		for j in np.arange(c):
-# 			if drawn_roi_path.contains_points([(j,i)]) == True:
-# 				 masked_dcm[i][j] = dcm_vid_framed[i][j]
-# 	g = rgb2gray(masked_dcm)
-# 	avg = np.mean(g)
-# 	gray_loop.append(avg)
-# 	index +=1
-# 	print(index)
-# print("--- %s seconds to run get_mask in a loop---" % (time.time() - start_time))
